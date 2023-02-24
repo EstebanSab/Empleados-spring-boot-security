ca915e4b384c3d69a9d1148616be59b1a313256d
Esteban <esabinio13@gmail.com>
2023-02-24 13:36:30 -0300

    eliminado de sobrantes

--- src/main/java/com/empleados/empleadosApi/controller/AuthenticationController.java
+++ src/main/java/com/empleados/empleadosApi/controller/AuthenticationController.java
@@ -1,25 +1,17 @@
 package com.empleados.empleadosApi.controller;
 
 
-import java.net.URI;
 import java.sql.Date;
 //import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.empleados.empleadosApi.model.Empleado;
-import com.empleados.empleadosApi.security.UserAuthenticationProvider;
-import com.empleados.empleadosApi.service.EmpleadoService;
+
 import com.empleados.empleadosApi.dto.CredentialsDto;
-import com.empleados.empleadosApi.dto.EmpleadoDto;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,26 +26,14 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @RequestMapping("/v1")
 public class AuthenticationController {
 
-    private  EmpleadoService empleadoService;
-    private  UserAuthenticationProvider userAuthenticationProvider;
-
-    @Autowired
-    public AuthenticationController(EmpleadoService empleadoService,
-                                    UserAuthenticationProvider userAuthenticationProvider) {
-        this.empleadoService = empleadoService;
-        this.userAuthenticationProvider = userAuthenticationProvider;
-     }
-
+ 
     @PostMapping("/login")
     public String signIn(@RequestBody CredentialsDto empleado) {
-        //return userAuthenticationProvider.createToken(empleado.getLogin());
-        //return ResponseEntity.ok(empleado);
-        
-String token = getJWTToken(empleado.getLogin());
-//User user = new User();
-//user.setUser(username);
-//user.setToken(token);		
-return token;
+       String token = getJWTToken(empleado.getLogin());
+        //User user = new User();
+        //user.setUser(username);
+        //user.setToken(token);		
+    return token;
 }
 
 private String getJWTToken(String username) {
@@ -77,19 +57,5 @@ String token = Jwts
 return "Bearer " + token;
 }
 
-    }
+}
 
-    /* 
-    @PostMapping("/signUp")
-    public ResponseEntity<EmpleadoDto> signUp(@RequestBody @Valid SignUpDto empleado) {
-        EmpleadoDto createdEmpleado = this.empleadoService.signUp(empleado);
-        return ResponseEntity.created(URI.create("/users/" + createdEmpleado.getId() + "/profile")).body(createdEmpleado);
-    }
-
-    @PostMapping("/signOut")
-    public ResponseEntity<Void> signOut(@AuthenticationPrincipal UserDto user) {
-        SecurityContextHolder.clearContext();
-        return ResponseEntity.noContent().build();
-    }
-    
-}*/
--- src/main/java/com/empleados/empleadosApi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/empleados/empleadosApi/security/ApplicationSecurityConfig.java
@@ -1,23 +1,12 @@
 package com.empleados.empleadosApi.security;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
@@ -26,53 +15,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final PasswordEncoder passwordEncoder;  
-    private final UserAuthenticationEntryPoint userAuthenticationEntryPoint;
-    private final UserAuthenticationProvider userAuthenticationProvider;
     
-    @Autowired
-    public ApplicationSecurityConfig(UserAuthenticationEntryPoint userAuthenticationEntryPoint,
-                          UserAuthenticationProvider userAuthenticationProvider,
-                          PasswordEncoder passwordEncoder) {
-        this.userAuthenticationEntryPoint = userAuthenticationEntryPoint;
-        this.userAuthenticationProvider = userAuthenticationProvider;
-        this.passwordEncoder=passwordEncoder;
-    }
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       /*http
-            // manejo de token xsrf token
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())     
-                .and()
-                .authorizeRequests()
-                .antMatchers("/")
-                .permitAll()
-                //las links que comienzen con api/ 
-                //deben ser user con role ADMIN
-                //.antMatchers("/api/**").hasRole("ADMIN")
-                //.antMatchers(HttpMethod.DELETE, "/api/empleados/**").hasAuthority("empleados:read")
-                //.antMatchers(HttpMethod.POST, "/api/empleados/**").hasAuthority("empleado:write")
-                //.antMatchers(HttpMethod.PUT, "/api/empleados/i/**").hasAuthority("empleado:write")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-                
-
-                http
-                .exceptionHandling().authenticationEntryPoint(userAuthenticationEntryPoint)
-                .and()
-                .addFilterBefore(new UsernamePasswordAuthFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new JwtAuthFilter(userAuthenticationProvider), UsernamePasswordAuthFilter.class)
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/v1/signIn", "/v1/signUp").permitAll()
-                .anyRequest().authenticated();
-  */
                 http.csrf().disable()
 				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 				.authorizeRequests()
@@ -81,48 +28,4 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   
             }   
     
-
-	
-//version 2.7.0 o superior
-/*
-@Bean
-protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
- http
-        .authorizeRequest()
-	.antMatchers("/").permitAll().
-	.antMatchers("/").authenticated();
-return http.build();
-}
-*/
-
-
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-
-     
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("12"))
-                //Seteamos el rol que tendra el usuario
-                .roles("ADMIN") // ROLE_ADMIN
-                .authorities("empleado:read","empleado:write")
-                .build();
-
-
-        UserDetails prohibido = User.builder()
-                .username("prohibido")
-                .password(passwordEncoder.encode("123"))
-                .roles("USER") 
-                .authorities("empleado:none")
-                .build();
-
-                return new InMemoryUserDetailsManager(
-                    admin,
-                    prohibido
-            );
-    
         }
-    
-}
